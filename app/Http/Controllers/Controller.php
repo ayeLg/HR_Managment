a03fd5a6114d0a582c78d5cb06dcd84bc460f550
Aung Thu <46448517+ayeLg@users.noreply.github.com>
2023-06-03 00:30:05 +0630

    Authentication

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,20 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function successReqonse($data,$message) {
+        return response()->json([
+            'error' => false,
+            'message' => $message,
+            'data' => $data,
+        ]);
+    }
+
+    public function errorResponse($message, $data = []) {
+        return response()->json([
+            'error' => true,
+            'message' => $message,
+            'data' => $data,
+        ]);
+    }
 }
